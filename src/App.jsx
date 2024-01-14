e36d2e025773c49de31b5473066bf5e8039006f5
Hyba <hibasafsafy@gmail.com>
2024-01-14 16:34:24 +0100

    finish tuto

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Task from "./components/Task";
 import TaskForm from "./components/TaskForm";
 
 function App() {
-
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
@@ -13,34 +12,60 @@ function App() {
   }, [tasks]);
 
   useEffect(() => {
-    const tasks_ = JSON.parse(localStorage.getItem('tasks')) || [];
-    setTasks(tasks_);
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    setTasks(storedTasks);
   }, []);
 
   const addTask = (name) => {
-    setTasks([...tasks, { id: Date.now(), name, done: false }])
-  }
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { id: Date.now(), name, done: false },
+    ]);
+  };
 
-  //no comprendo 
   const checkTask = (taskId) => {
-    const updatedTasks = tasks.map(task => {
-      if (task.id === taskId) {
-        return { ...task, done: !task.done }
-      }
-      return task
-    })
-    setTasks(updatedTasks)
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, done: !task.done } : task
+      )
+    );
+  };
+
+  const removeItem = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
+  const renameTask = (taskId, newName) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, name: newName } : task
+      )
+    );
   };
 
 
-  const numberComplete = tasks.filter(t => t.done).length;
+  const numberComplete = tasks.filter((t) => t.done).length;
   const numberTotal = tasks.length;
+  const getMessage = () => {
+    const percentage = (numberComplete / numberTotal) * 100;
+    if (percentage === 0) {
+      return "Try to do at least one! 🙏🏼";
+    }
+    if (percentage === 100) {
+      return "Nice job for today! ✅";
+    }
+    if (percentage >= 1 && percentage < 100){
+      return "keep it going 💪🏼";
+    }
+    return "Welcome to your AGENDA ! You can ad your tasks for today 📒";
+  };
 
   return (
     <div className="main">
       <h1>
         {numberComplete}/{numberTotal} complete
       </h1>
+      <h2>{getMessage()}</h2>
       <TaskForm onAdd={addTask} />
       {tasks.map((task) => (
         //no comprendo
@@ -48,6 +73,8 @@ function App() {
           key={task.id}
           {...task}
           checkTask={checkTask}
+          onTrash={() => removeItem(task.id)}
+          onRename={newName => renameTask(task.id, newName) }
         />
       ))}
     </div>
